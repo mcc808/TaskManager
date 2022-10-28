985fd44eeb6bfdb5b10748e9d767beda15efa089
mcc808 <112995753+mcc808@users.noreply.github.com>
2022-10-28 02:25:35 -0700

    commit again

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+var iconImportant = "fas fa-exclamation";
+var iconNonImportant = "far fa-exclamation";
+var isImportant = false;
+var isVisible = true;
+
+function toggleImportant(){
+    if(isImportant){
+        //should not be important
+        $("#iImportant").removeClass(iconImportant);
+        $("#iImportant").addClass(iconNonImportant);
+        isImportant = false;
+    }else{
+        //should be important
+        $("#iImportant").removeClass(iconNonImportant);
+        $("#iImportant").addClass(iconImportant);
+        isImportant = true;
+    }
+}
+
+function toggleForm(){
+    if(isVisible){
+        $(".form").hide();
+        isVisible = false;
+    }else{
+        $(".form").show();
+        isVisible = true; 
+    }
+}
+
 function saveTask(){
     //console.log("Saving task...");
     let title = $('#txtTitle').val();
